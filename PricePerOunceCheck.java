b7230b20051f6847eed69f2dd5dda565002f6fee
James Traver <jtraverjr@gmail.com>
2013-02-10 00:40:55 -0600

    Initial commit
    Initial program commit

---
+++ PricePerOunceCheck.java
@@ -0,0 +1,44 @@
+package priceperouncecheck;
+import java.util.Scanner;
+
+/**
+ *
+ * @author James Traver
+ * james [at] codeforcoffee [dot] org
+ * github.com/code-for-coffee
+ * 
+ * This software is released under the GPL License.
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ */
+
+public class PricePerOunceCheck {
+
+    /*
+     * This application is designed to allow a user to enter:
+     * 1 product's price and ounce and another product's price OR ounce
+     * It will then alert the user as to which is the better buy.
+     */
+    
+    public static String RatioCalculate(double firstProductPrice, 
+                                        double firstProductOunce, 
+                                        double secondProductPrice, 
+                                        double secondProductOunce) {
+        /* 
+         * @params firstProductPrice is the price of the first product
+         * @params firstProductOunce are how many ounces are in the first product
+         * @params secondProductPrice is the price of the first product
+         * @params secondProductOunce are how many ounces are in the first product
+         */
+        
+        // declare return data
+        String whichIsBetterProduct = "";
+        
+        // return which product is the better buy
+        return whichIsBetterProduct;
+        
+    }
+    public static void main(String[] args) {
+        // gather user input here
+    }
+}
