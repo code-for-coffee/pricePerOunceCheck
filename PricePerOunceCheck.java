b1aad4cc2923e5cda536b704fbd85d1a2c94e0e8
James Traver <jtraverjr@gmail.com>
2013-02-10 01:46:04 -0600

    more work

--- PricePerOunceCheck.java
+++ PricePerOunceCheck.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
  * This software is released under the GPL License.
  * http://www.gnu.org/licenses/gpl.html
  * 
+ * 
  */
 
 public class PricePerOunceCheck {
@@ -31,18 +32,25 @@ public class PricePerOunceCheck {
          * @params secondProductOunce are how many ounces are in the first product
          */
         
-        // declare return data
-        String whichIsBetterProduct = "";
+        // declare variables to be used in RatioCalculate()
+        String whichIsBetterProduct[] = { "First Product with a price of $ ",
+                                          "Second Product with a price of $ " };
+        double firstProduct           = firstProductPrice / firstProductOunce;
+        double secondProduct          = secondProductPrice / secondProductOunce;
         
-        // return which product is the better buy
-        return whichIsBetterProduct;
+        if (firstProduct > secondProduct) {
+            return whichIsBetterProduct[0] + firstProductPrice;
+        } else {
+            return whichIsBetterProduct[1] + secondProductPrice;
+        }
     }
     
     public static void main(String[] args) {
         
         // define variables to be used in main()
         final int ARRAY_LENGTH  = 4;
-        double productInput[]   = new double[ARRAY_LENGTH];
+        //String productInput[] = new String[ARRAY_LENGTH];     # will be used once error handling is added
+        double productData[]    = new double[ARRAY_LENGTH];
         String appName          = "PricePerOunceCheck";
         String appDescription   = "An app to find what is a better buy between price/Oz variations.";
         String appGuide[]       = { "Please enter the first product's price: ", 
@@ -59,10 +67,19 @@ public class PricePerOunceCheck {
         
         // run a loop to gather this data
         for (int i = 0; i < 4; i++) {
-            System.out.append(appGuide[i]);
-            productInput[i] = userInput.nextByte();
-        }      
+            
+           System.out.append(appGuide[i]);
+           productData[i] = userInput.nextDouble();
+                      
+           /* we will add data validation later           
+           boolean isNaNTest;
+           double testThisNumber;
+           isNaNTest = Double.isNaN(testThisNumber);
+           System.out.print(isNaNTest);
+           */
+        }    
         
+              
         /* 
          * this is included just for testing to make sure valid data was entered
         
